aeb284faf55dadccf9c59719a8f36eae38bfbfd2
Taylor Mighall <taylormighall@gmail.com>
2018-04-23 17:50:42 +0100

    Added comments

--- platforms/browser/www/choose-the-spelling.js
+++ platforms/browser/www/choose-the-spelling.js
@@ -1,6 +1,6 @@
 var spellings = [["achieve", "acheive", "acheeve", "achyve"], ["business","buissines","businnes","buisinness"], ["successful","sucksessful","sucsessful","succesful"], ["beautiful","beeutiful", "beatiful","beautful"], ["believe", "beleve","beleive","belive"], ["siege", "seige", "seege","sieje"],["cycling", "cycwling", "cyculing", "sycling"],["dyslexia","dislexia","dylecsia","dislecsia"], ["tomorrow", "tommorow", "tommorrow", "tomorow"], ["remember","remmember","rember","remeber"]];
-
-function getRandomSpellings(){
+// Above is the 2 dimensional Array containing the sets of correct and incorrectly spelt words
+function getRandomSpellings(){ // This function calls the getRandomSpelling spelling function to get a random list of 4 words. It then mixes them up and assigns them each to a button.
 
 var RandomSpelling = getRandomSpelling();
 
@@ -12,9 +12,13 @@ document.getElementById("button4").textContent = RandomOrder[3];
 
 };
 
-function checkAnswer1(){
-  var answer = document.getElementById("button1").textContent.toString();
+var getRandomSpelling = function () { //This function uses the Math.random function to select a 2nd level array
+    return spellings[Math.floor(Math.random() * spellings.length )];
+};
 
+function checkAnswer1(){ //The checkAnswer functions correspond to a button. They take the word on the pressed button and compare it to the list of correct spellings.
+  var answer = document.getElementById("button1").textContent.toString(); // If correct it generates another set of words aand displays the "good job" text.
+                                                                          // If incorrect it displays the "incorrect" text and lets the user choose again.
   if  (answer == "achieve" || answer == "business" || answer == "successful"|| answer == "beautiful"||answer == "believe"|| answer == "siege" || answer == "cycling" || answer == "remember" || answer == "tomorrow" || answer == "dyslexia") { document.getElementById("answercorrect").textContent = "Incorrect, please try again.";
   getRandomSpellings();
   document.getElementById("answercorrect").textContent = "Good job, here's another";
@@ -63,10 +67,4 @@ function checkAnswer4(){
  document.getElementById("answercorrect").style.color = "#DD4132";
 
   }
-};
-
-
-
-var getRandomSpelling = function () {
-    return spellings[Math.floor(Math.random() * spellings.length )];
 };
